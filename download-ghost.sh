a675ff0b2143c38539858e4f0f560a145bd74c1d
Welton Rodrigo Torres Nascimento <rodrigo@familianascimento.org>
2016-09-09 19:21:55 -0400

    Script to download ghostscript

---
+++ download-ghost.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs919/ghostscript-9.19-linux-x86_64.tgz
+
+tar xzf ghostscript-9.19-linux-x86_64.tgz
+mv ghostscript-9.19-linux-x86_64/gs* .
