2e7a2e27ba50a8f80d026ab622fb7468879ec35f
Welton Rodrigo Torres Nascimento <rodrigo@familianascimento.org>
2016-09-09 19:31:18 -0400

    Automatically find right location for PDFA_def.ps

--- convert.sh
+++ convert.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #set -x
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 INFILE="${1}"
 OUTFILE="PDFA-${1}"
 
-./gs-919-linux_x86_64 -dPDFA=1 -dNOOUTERSAVE -sProcessColorModel=DeviceRGB -sDEVICE=pdfwrite -o $OUTFILE PDFA_def.ps -dPDFACompatibilityPolicy=1 $INFILE
+./gs-919-linux_x86_64 -dPDFA=1 -dNOOUTERSAVE -sProcessColorModel=DeviceRGB -sDEVICE=pdfwrite -o $OUTFILE ${DIR}/PDFA_def.ps -dPDFACompatibilityPolicy=1 $INFILE
